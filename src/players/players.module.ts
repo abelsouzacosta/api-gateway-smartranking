da341cac06837d14670feb5d1ead92a0169de279
junior <abelsouzacosta@gmail.com>
2022-11-08 06:54:52 -0300

    chore(app): adding service

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { Services } from 'src/common/enum/service-names.enum';
+import { RABBITMQ_URL } from 'src/config';
+import { Queues } from 'src/common/enum/queue-names.enum';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: Services.PLAYERS_SERVICE,
+        transport: Transport.RMQ,
+        options: {
+          urls: [RABBITMQ_URL],
+          queue: Queues.PLAYERS,
+        },
+      },
+    ]),
+  ],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
